263b3a9d315feb8d6a4f6ebb11b26c0eac4a3102
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-10-06 22:53:50 +0530

    styles modified to support mobile layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -340,11 +340,11 @@
 	
 	<!-- Avatar Icon -->
 	<div class="dropdown dropdown-end ml-2">
-		<label tabindex="0" class="btn btn-ghost btn-circle avatar" aria-label="User menu">
-			<div class="w-10 rounded-full">
-				<img alt="User Avatar" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    	<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar border-2 border-gray-400">
+			<div class="bg-neutral text-neutral-content w-16 pt-1 rounded-full">
+				<span class="text-xl">AI</span>
 			</div>
-		</label>
+		</div>
 		<ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
 			<li><a href="/profile" class="justify-between">Profile <span class="badge">New</span></a></li>
 			<li><a href="#">Settings</a></li>
